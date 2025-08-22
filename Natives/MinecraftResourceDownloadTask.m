525844dd5fbf7bc8ffc4418f55e18e4a0b9f04f3
congcq <aiskai2007@gmail.com>
2025-08-22 18:13:33 +0700

    Fix: True -> true

--- Natives/MinecraftResourceDownloadTask.m
+++ Natives/MinecraftResourceDownloadTask.m
@@ -315,7 +315,7 @@
 // Check if the account has permission to download
 - (BOOL)checkAccessWithDialog:(BOOL)show {
     // Enable all permission for local account.
-    return True;
+    return true;
 }
 
 // Check SHA of the file
