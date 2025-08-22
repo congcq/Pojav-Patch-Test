60d6d6286628fb0326c274e4f62ea855c943a254
congcq <aiskai2007@gmail.com>
2025-08-22 17:46:38 +0700

    Feat(config): add config for mges

--- Natives/LauncherPrefMGESConfigViewController.m
+++ Natives/LauncherPrefMGESConfigViewController.m
@@ -3,11 +3,25 @@
 #import "LauncherPreferencesViewController.h"
 #import "LauncherPrefMGESConfigViewController.h"
 
+@interface LauncherPrefMGESConfigViewController ()<UIContextMenuInteractionDelegate>
+@property(nonatomic) NSMultableDictionary<NSNumber *, NSMultableArray *> *mgesConfigs;
+@property(nonatomic) NSMultableDictionary<NSNumber *> *options;
+@end
+
 @implementation LauncherPrefMGESConfigViewController
 
 - (void)viewDidLoad {
    [super viewDidLoad];
-   //[self setTitle:localize(@"preference.title.mges_config", nil)];
-   self.title = @"MobileGlues Config";
+   [self setTitle:NSLocalizedString(@"preference.title.mges_config", nil)];
+   //self.title = @"MobileGlues Config";
 }
+   self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleInsetGrouped];
+
+   self.mgesConfigs = @{
+      @(0):
+         @[@"preference.mges_config.option1",
+         @"preference.mges_config.option2",
+         @"preference.mges_config.option3"
+         ]
+   }.mutableCopy;
 @end
